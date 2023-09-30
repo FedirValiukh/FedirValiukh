c5f41b9fcccc624219122ad3be68cca4de1ea7bf
Prince Alladin <fedirzpua@gmail.om>
2023-09-30 21:40:07 +0300

    init commit

---
+++ src/Task1.java
@@ -0,0 +1,50 @@
+public class Task1 {
+    /* Метод main в java це перший метод, про який дізнаються початківці, бо він є
+    точкою входу в програмування на Java. Цей метод може утримувати код для виклику
+     або виконання інших методів та його можна вкласти в будьякий клас, який є частиною програми.
+   */
+    public static void main(String[] args) {
+
+        String name = "Fedir";
+        int age = 38;
+        byte age1 = 4;
+        short age3 = 5;
+        int count = 6;
+        byte bytCount =3;
+        String name1 = "Earth";
+        String name2 = "Sun";
+        String strName = "Moon";
+        char k = 69;
+        char j = 65;
+        char d = 82;
+        char g = 84;
+        char l = 72;
+        char h = 'd';
+        long count1 = 40;
+        System.out.println("Mi name is " + name + ". I am a human." );
+        System.out.println("I leave on the planet " + name1+ ".");
+        System.out.println("The  " +name1 +  "is"  + age1 + "billion year old.");
+        System.out.println("Our nearest star is the " + name2 + "." );
+        System.out.println("The " + name2 +  " is "  + age1 +"."+ age3 + " billion year old.");
+        System.out.println("There are " + age1 + " oceans on planet " + name1 + "."  );
+        System.out.println("There are " + count + " contіnents on planet " + name1 + ".");
+        System.out.println("The " + strName + "is a satellite of the " + name1+ ".");
+        System.out.println("The " + name1 + " is the " + bytCount + " planet from the " + name2 + ".");
+        System.out.println("Planet " + name1 + " is inhabited by " + age1 + " races of people.");
+
+        System.out.println(k);
+        System.out.println(j);
+        System.out.println(d);
+        System.out.println(g);
+        System.out.println(l);
+
+        System.out.println("========================");
+
+
+        System.out.println(h);
+
+        System.out.println(h * h * h * h * count1 + " years old!" );
+
+
+    }
+}
